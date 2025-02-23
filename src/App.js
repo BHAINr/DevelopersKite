6818ceca59e6778158bc92cb911dc0bf15ac863a
navedkhan63705 <123861289+navedkhan63705@users.noreply.github.com>
2025-02-23 16:05:17 +0000

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './Components/JavaScript/Home';
 import Navbar from './Components/JavaScript/Navbar';
 import AWSServicesPage from './Components/JavaScript/AWSServicesPage';
 import AboutUs from './Components/JavaScript/AboutUs';
+import ContactForm from './Components/JavaScript/ContactForm';
+import ChatBox from './Components/JavaScript/ChatBox';
+ import CloudService from './Components/JavaScript/CloudService';
+
 
 
 function App() {
@@ -15,7 +19,10 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/awsservices" element={<AWSServicesPage/>} />
         <Route path="/about" element={<AboutUs/>} />
+        <Route path="/contact" element={<ContactForm/>} />
+        <Route path="/cd" element={<CloudService/>} />
       </Routes>
+      <ChatBox/>
     </Router>
    
   );
--- src/Components/JavaScript/Home.js
+++ src/Components/JavaScript/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Homeservices from './Homeservices';
+import { ChartBar } from 'lucide-react';
 
 const Home = () => {
   return (
     <div>
       <Homeservices/>
+       <ChartBar/>
     </div>
   )
 }
